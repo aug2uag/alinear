d6c9fd2d214161b2c6d813d32efb117991d6ae28
Reza Fatahi <aug2uag@gmail.com>
2021-09-19 23:01:43 -0700

    add tests and TODO

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ This smart contract is intended to interface with the alinear dApp that handles 
 In its place, the token is designed to represent a discrete unit of function pertaining to a task managed by the dApp. A token creator will bind the token to an operation for their internal Human Resource hiring affairs, all the details of which are handled off-chain. __The purpose and value of this system is to ensure the bounty is paid contingent to the bounty hunter having reported and contributed to an employee who remains with the company long enough for the bounty hunter to be awarded in full__. Therefore, the purpose of the time-lock mechanism is to ensure a business doesn't have to assume risks of paying others for employees that turn out to be unqualified or who do not stay.
 
 
+TODO:
+- add `Ownable` and lock `total_approvals` after first set (only once; mutex) by the owner
+- ensure amount and transfer with `pay_bounty_hunter` and `nft_is_approved`
+- remove `pay_bounty_hunter` and handle payouts on approval (`nft_is_approved` only)
+- improve test coverage to include full transfer schedule
+- harden revoke, ensure no deposit and balance is lost
+- audit (static and dynamic analysis), publish on mainet
--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ NOTES:
   - To prevent the deployed contract from being modified or deleted, it should not have any access
     keys on its account.
 */
+
 use near_contract_standards::non_fungible_token::metadata::{
     NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
 };
@@ -24,27 +25,24 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LazyOption;
 use near_sdk::json_types::ValidAccountId;
 use near_sdk::{
-    env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
+    env, near_bindgen, AccountId, Balance, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
 };
 use near_sdk::MockedBlockchain;
 
 near_sdk::setup_alloc!();
-
-pub struct Milestones {
-    milestone1: str
-}
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
-    value: u128,
     bounty_hunter: ValidAccountId,
+    balance: Balance,
+    total_approvals: u16
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
-const ONE_NEAR_AS_YOCTO: u128 = 1_000_000_000_000_000_000_000_000; // 1 $NEAR as yoctoNEAR
+const ONE_NEAR_AS_YOCTO: Balance = 1_000_000_000_000_000_000_000_000; // 1 $NEAR as yoctoNEAR
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
@@ -59,11 +57,12 @@ enum StorageKey {
 impl Contract {
     /// Initializes the contract owned by `owner_id` with
     /// default metadata (for example purposes only).
+    /// sets default value to token
     #[init]
     pub fn new_default_meta(owner_id: ValidAccountId) -> Self {
-        let amount = env::attached_deposit();
-        println!("{:?} = amount", amount);
-        if amount < 1 {
+        let balance: Balance = env::attached_deposit();
+        println!("{:?} = balance", balance);
+        if balance < 1 {
             panic!("not adequate size bounty");
         }
         Self::new(
@@ -77,27 +76,29 @@ impl Contract {
                 reference: None,
                 reference_hash: None,
             },
-            amount
+            balance
         )
 
 
     }
 
     #[init]
-    pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata, amount: u128) -> Self {
+    pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata, balance: Balance) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         metadata.assert_valid();
+        let key: ValidAccountId = serde_json::from_str("\"alice.near\"").unwrap();
         Self {
             tokens: NonFungibleToken::new(
                 StorageKey::NonFungibleToken,
-                owner_id.clone(),
+                owner_id,
                 Some(StorageKey::TokenMetadata),
                 Some(StorageKey::Enumeration),
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
-            value: amount,
-            bounty_hunter: owner_id
+            bounty_hunter: key,
+            balance: balance,
+            total_approvals: 3  // TODO: make this 0
         }
     }
 
@@ -127,8 +128,21 @@ impl Contract {
     /// generates a value backed token that is time locked for release to the
     /// bounty hunter.
     pub fn get_value(
-        &mut self) -> u128 {
-        self.value
+        &mut self,
+    ) -> Balance {
+        self.balance
+    }
+
+    /// Set number of approvals
+    /// 
+    /// This value determines the proportion of the payout and initiated by
+    /// system/owner calling `nft_is_approved` on a pre-determined schedule
+    /// defined off-chain before and/or during token existence.
+    pub fn set_approval_schedule(
+        &mut self,
+        approvals: u16
+    ) {
+        self.total_approvals = approvals
     }
 
     /// Add bounty hunter
@@ -150,19 +164,18 @@ impl Contract {
         self.bounty_hunter
     }
 
-
     /// Payout at milestone
     ///
     /// The bounty hunter is paid out according to a schedule maintained
     /// off chain; the proportion (of 100) is sent, evaluated if there is
-    /// enough or remaining value is sent
+    /// enough or remaining value is sent.
     pub fn pay_bounty_hunter(
         self,
         proportion: u128) -> Promise {
-        let value = self.value * proportion;
-        let yocto: u128 = (value * ONE_NEAR_AS_YOCTO)/100;
+        let value = self.balance * proportion;
+        println!("{:?} = value", value);
         Promise::new(self.bounty_hunter.to_string())
-            .transfer(yocto)
+            .transfer(value)
     }
 
 }
@@ -253,7 +266,7 @@ mod tests {
     }
 
     #[test]
-    fn test_transfer() {
+    fn add_bounty_hunter() {
         let mut context = get_context(accounts(0));
         testing_env!(context.build());
         let mut contract = Contract::new_default_meta(accounts(0).into());
@@ -263,30 +276,42 @@ mod tests {
             .attached_deposit(MINT_STORAGE_COST)
             .predecessor_account_id(accounts(0))
             .build());
+
         let token_id = "0".to_string();
         contract.nft_mint(token_id.clone(), accounts(0), sample_token_metadata());
+        
+        let bob = accounts(1);
+        contract.set_bounty_hunter(bob.clone());
+
+        let bounty_hunter = contract.get_bounty_hunter();
+        assert_eq!(bounty_hunter, bob);
+
+    }
+
+    #[test]
+    fn pay_bounty_hunter() {
+        let mut context = get_context(accounts(0));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default_meta(accounts(0).into());
 
         testing_env!(context
             .storage_usage(env::storage_usage())
-            .attached_deposit(1)
+            .attached_deposit(MINT_STORAGE_COST)
             .predecessor_account_id(accounts(0))
             .build());
-        contract.nft_transfer(accounts(1), token_id.clone(), None, None);
 
-        testing_env!(context
-            .storage_usage(env::storage_usage())
-            .account_balance(env::account_balance())
-            .is_view(true)
-            .attached_deposit(0)
-            .build());
-        if let Some(token) = contract.nft_token(token_id.clone()) {
-            assert_eq!(token.token_id, token_id);
-            assert_eq!(token.owner_id, accounts(1).to_string());
-            assert_eq!(token.metadata.unwrap(), sample_token_metadata());
-            assert_eq!(token.approved_account_ids.unwrap(), HashMap::new());
-        } else {
-            panic!("token not correctly created, or not found by nft_token");
-        }
+        let token_id = "0".to_string();
+        contract.nft_mint(token_id.clone(), accounts(0), sample_token_metadata());
+        
+        let bob = accounts(1);
+        contract.set_bounty_hunter(bob.clone());
+
+        let balance_initial = &contract.get_value();
+        contract.pay_bounty_hunter(100);
+        assert!(balance_initial > &0);
+        // let balance_final = ;
+        // assert!(balance_initial > contract.get_value());
+
     }
 
     #[test]
