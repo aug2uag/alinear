412f5ca4ee6c9cb2f8147de406f7d22ad0907976
Nesim <nesim.engineer@gmail.com>
2021-09-19 23:30:10 -0700

    update token name and symbol

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,6 @@ pub struct Contract {
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
-const ONE_NEAR_AS_YOCTO: Balance = 1_000_000_000_000_000_000_000_000; // 1 $NEAR as yoctoNEAR
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
@@ -57,7 +56,7 @@ enum StorageKey {
 impl Contract {
     /// Initializes the contract owned by `owner_id` with
     /// default metadata (for example purposes only).
-    /// sets default value to token
+    /// sets bounty value to token
     #[init]
     pub fn new_default_meta(owner_id: ValidAccountId) -> Self {
         let balance: Balance = env::attached_deposit();
@@ -69,8 +68,8 @@ impl Contract {
             owner_id,
             NFTContractMetadata {
                 spec: NFT_METADATA_SPEC.to_string(),
-                name: "Example NEAR non-fungible token".to_string(),
-                symbol: "EXAMPLE".to_string(),
+                name: "ALINEAR HR TOKEN".to_string(),
+                symbol: "NEAHR".to_string(),
                 icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
                 base_uri: None,
                 reference: None,
@@ -98,7 +97,7 @@ impl Contract {
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
             bounty_hunter: key,
             balance: balance,
-            total_approvals: 3  // TODO: make this 0
+            total_approvals: 3  // TODO: make this 0 and set from function call
         }
     }
 
@@ -117,7 +116,6 @@ impl Contract {
         receiver_id: ValidAccountId,
         token_metadata: TokenMetadata,
     ) -> Token {
-        // TODO: receiver_id is always protocol
         println!("{:?} = receiver_id", receiver_id);
         self.tokens.mint(token_id, receiver_id, Some(token_metadata))
     }
