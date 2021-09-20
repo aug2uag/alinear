640cefc9095e7dd511c96509424140143b3d722a
Reza Fatahi <aug2uag@gmail.com>
2021-09-19 19:35:32 -0700

    smart contract developed

--- README.md
+++ README.md
@@ -1,36 +1,9 @@
 # alinear
 
-defi insurance platform via NEAR protocol
+> HR time-lock bounty hunting smart contract
+
+This smart contract is intended to interface with the alinear dApp that handles much of the logic off-chain. For example, the smart contract doesn't maintain the schedule of payouts-- this would need additional struct and storage params that currently seem to be unnecessary.
+
+In its place, the token is designed to represent a discrete unit of function pertaining to a task managed by the dApp. A token creator will bind the token to an operation for their internal Human Resource hiring affairs, all the details of which are handled off-chain. __The purpose and value of this system is to ensure the bounty is paid contingent to the bounty hunter having reported and contributed to an employee who remains with the company long enough for the bounty hunter to be awarded in full__. Therefore, the purpose of the time-lock mechanism is to ensure a business doesn't have to assume risks of paying others for employees that turn out to be unqualified or who do not stay.
 
 
-### Overview
-
-1. NFT architecture
-	* form-fit factor:
-		- claims are unique
-		- maintains isolation
-		- facilitates staking operations
-	* novelty factor
-		- no one utilizing this method
-		- others utilizing with ERC20
-	* security factor
-		- majority of vulnerabilities related to ERC20
-		- potential form factor as more efficient code
-
-2. Mechanism
-	* claim generation
-		- user generates claim
-	* claim evaluation
-		- assessment via BAND or similar
-	* claim offer and staking
-		- protocol remains the owner
-	* claim staking
-		- up to _n_ amount of stakers eligible
-	* AMM (Automatic Market Makers)
-		- staked funds participate in AMM
-	* claim execution
-		- evaluation of feasibility based on staking Hx
-		- evaluation of feasibility based on underwriter/assessor
-		- proportional calculation determination and payout to claimant
-	* claim cleanup
-		- burn token and payout to stakees
--- src/lib.rs
+++ src/lib.rs
@@ -30,14 +30,21 @@ use near_sdk::MockedBlockchain;
 
 near_sdk::setup_alloc!();
 
+pub struct Milestones {
+    milestone1: str
+}
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
+    value: u128,
+    bounty_hunter: ValidAccountId,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+const ONE_NEAR_AS_YOCTO: u128 = 1_000_000_000_000_000_000_000_000; // 1 $NEAR as yoctoNEAR
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
@@ -54,6 +61,11 @@ impl Contract {
     /// default metadata (for example purposes only).
     #[init]
     pub fn new_default_meta(owner_id: ValidAccountId) -> Self {
+        let amount = env::attached_deposit();
+        println!("{:?} = amount", amount);
+        if amount < 1 {
+            panic!("not adequate size bounty");
+        }
         Self::new(
             owner_id,
             NFTContractMetadata {
@@ -65,22 +77,27 @@ impl Contract {
                 reference: None,
                 reference_hash: None,
             },
+            amount
         )
+
+
     }
 
     #[init]
-    pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata) -> Self {
+    pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata, amount: u128) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         metadata.assert_valid();
         Self {
             tokens: NonFungibleToken::new(
                 StorageKey::NonFungibleToken,
-                owner_id,
+                owner_id.clone(),
                 Some(StorageKey::TokenMetadata),
                 Some(StorageKey::Enumeration),
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            value: amount,
+            bounty_hunter: owner_id
         }
     }
 
@@ -99,8 +116,55 @@ impl Contract {
         receiver_id: ValidAccountId,
         token_metadata: TokenMetadata,
     ) -> Token {
+        // TODO: receiver_id is always protocol
+        println!("{:?} = receiver_id", receiver_id);
         self.tokens.mint(token_id, receiver_id, Some(token_metadata))
     }
+
+    /// Get value of the contract
+    /// 
+    /// This value is set by the token creator as the bounty of the contract
+    /// generates a value backed token that is time locked for release to the
+    /// bounty hunter.
+    pub fn get_value(
+        &mut self) -> u128 {
+        self.value
+    }
+
+    /// Add bounty hunter
+    ///
+    /// The bounty hunter is the one who is eligible for this bounty in case
+    /// the time lock requirements have been met
+    pub fn set_bounty_hunter(
+        &mut self,
+        bounty_hunter: ValidAccountId) {
+        self.bounty_hunter = bounty_hunter
+    }
+
+    /// Return bounty hunter
+    ///
+    /// The bounty hunter is the one who is eligible for this bounty in case
+    /// the time lock requirements have been met
+    pub fn get_bounty_hunter(
+        self) -> ValidAccountId {
+        self.bounty_hunter
+    }
+
+
+    /// Payout at milestone
+    ///
+    /// The bounty hunter is paid out according to a schedule maintained
+    /// off chain; the proportion (of 100) is sent, evaluated if there is
+    /// enough or remaining value is sent
+    pub fn pay_bounty_hunter(
+        self,
+        proportion: u128) -> Promise {
+        let value = self.value * proportion;
+        let yocto: u128 = (value * ONE_NEAR_AS_YOCTO)/100;
+        Promise::new(self.bounty_hunter.to_string())
+            .transfer(yocto)
+    }
+
 }
 
 near_contract_standards::impl_non_fungible_token_core!(Contract, tokens);
@@ -122,11 +186,13 @@ mod tests {
     use super::*;
 
     const MINT_STORAGE_COST: u128 = 5870000000000000000000;
+    const DEPOSIT: u128 = 100;
 
     fn get_context(predecessor_account_id: ValidAccountId) -> VMContextBuilder {
         let mut builder = VMContextBuilder::new();
         builder
             .current_account_id(accounts(0))
+            .attached_deposit(DEPOSIT)
             .signer_account_id(predecessor_account_id.clone())
             .predecessor_account_id(predecessor_account_id);
         builder
